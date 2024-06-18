c2f3998c4486a05601463640140400bdd4554ffc
Nizar-Osman <117791265+Nizar-Osman@users.noreply.github.com>
2024-06-18 17:42:52 -0400

    imported navbar from nextui

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import {Button} from '@nextui-org/button';
+import Header from "./components/header";
 
 export default function Home() {
   return (
-    <main>
-       <div>
-      <Button>Click me</Button>
-    </div>
-    </main>
+    <>
+      <Header />
+    </>
   );
 }
