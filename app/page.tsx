597e62a5f602da7b7cb1e9023afd18d79a10599f
Nizar-Osman <117791265+Nizar-Osman@users.noreply.github.com>
2024-06-18 19:24:44 -0400

    attatch class names to all respective elements + render a  placeholder image

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ export default function Home() {
     <>
       <Header />
         <main>
-          <h1> Aspiring React Developer</h1>
-          <p> Specializing In Frontend Creativity & React-Interactivity</p>
+          <img className="hero-img" src="https://picsum.photos/200/300" />
+          <h1 className="hero-title"> Aspiring React Developer</h1>
+          <p className="hero-p"> Specializing In Frontend Creativity & React-Interactivity</p>
         </main>
       <section>
-        <h2>About Me</h2>
-        <p> I am a self-taught developer with a passion for creating beautiful, interactive websites.
+        <h2 className="about-me-title">About Me</h2>
+        <p className="about-me-p"> I am a self-taught developer with a passion for creating beautiful, interactive websites.
           I started my journey with the fundamentals of HTML, CSS, and JavaScript, through the odin Project.
           I have 2 certifications in web development from freeCodeCamp ; Repsonsive Web Design and Datastructures and Algorithms.
           I also completed the Bob Ziroll React course on Scrimba, as an introduction into React.
@@ -19,6 +20,7 @@ export default function Home() {
           Why do I want to be in web development? I love the different stages of web development, from the expression of creatitivity while designing the website, to the logic and problem solving while coding the website.
           I have a knack for problem solving and I am always eager to learn new things.
           I am looking for an opportunity to grow and learn in a professional environment.
+  
         </p>
       </section>
     </>
