a5e52e1da5fd499afcdee98bdccabe83047dc15e
Nizar-Osman <117791265+Nizar-Osman@users.noreply.github.com>
2024-06-27 20:44:59 -0400

    Render hero section with coresponding css module

---
+++ app/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Hero.module.css'
+function Hero() {
+  return (
+    <>
+          <img className={styles.img} src="https://picsum.photos/200/300" />
+          <h1 className={styles.h1}> Express Your Brand Through Prestige & Function</h1>
+          <p className={styles.p}>We Specialize In Custom interactive application development. SAAS integration that will set you apart from the competition.
+          </p>
+        </>
+  )
+}
+
+export default Hero
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import About from "./components/AboutMe";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-        <main>
-          <img className="hero-img" src="https://picsum.photos/200/300" />
-          <h1 className="hero-title"> Express Your Brand Through Prestige & Function</h1>
-          <p className="hero-p">We Specialize In Custom interactive application development. SAAS integration that will set you apart from the competition.
-          </p>
-        </main>
+      <Hero />
       <About />
       
       <section> 
